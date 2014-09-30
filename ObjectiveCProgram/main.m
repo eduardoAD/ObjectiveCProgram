5a596e0e3ee616f29f262ff533c24a0b1f09ac53
Eduardo Alvarado Díaz <GLB-MXM0001@Eduardos-MacBook-Pro-2.local>
2014-09-30 18:19:48 -0500

    Initial Commit

---
+++ ObjectiveCProgram/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveCProgram
+//
+//  Created by Eduardo Alvarado Díaz on 9/30/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
